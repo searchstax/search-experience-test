7b49ba22e6e3d24e30747302ace221381294cca4
searchstaxkevin <admin@kevin-mbp.mynetworksettings.com>
2025-06-12 11:27:31 -0700

    Deploy fixes

--- search-experience-test-react/vite.config.ts
+++ search-experience-test-react/vite.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig(({ command, mode, isSsrBuild, isPreview }) => {
   if (command === 'build') {
     return {
@@ -23,7 +22,6 @@ export default defineConfig(({ command, mode, isSsrBuild, isPreview }) => {
           }
         }
       },
-      //base: '/modules/searchstax_studio_react_bar/dist/',
     }
   }
   else {
